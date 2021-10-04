52233d9b29ef6f97eab1a6cb40ac916348df6cb3
lkimilhol <lkimilhol@gmail.com>
2021-10-04 12:07:46 +0900

    Initial commit

---
+++ src/main/java/com/lkimilhol/kafkatoy/KafkatoyApplication.java
@@ -0,0 +1,13 @@
+package com.lkimilhol.kafkatoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lkimilhol/kafkatoy/KafkatoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkimilhol.kafkatoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkatoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
